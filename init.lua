2d7cf04ac6bfa84467313a4eec9c6239bfeaa4b4
Éric NICOLAS (ccjmne) <ccjmne@gmail.com>
2024-07-27 15:55:55 +0200

    Toggle format-on-save w/ <leader>tf

--- init.lua
+++ init.lua
@@ -685,9 +685,37 @@ require('lazy').setup({
         desc = '[F]ormat buffer',
       },
     },
+    init = function()
+      -- See https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
+      vim.api.nvim_create_user_command('FormatDisable', function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = 'Disable autoformat-on-save',
+        bang = true,
+      })
+      vim.api.nvim_create_user_command('FormatEnable', function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = 'Re-enable autoformat-on-save',
+      })
+      vim.keymap.set('n', '<leader>tf', function()
+        vim.g.disable_autoformat = not vim.g.disable_autoformat
+      end, { desc = '[T]oggle [F]ormat on save' })
+      -- map('n', '<leader>tf', vim.FormatDisable, { desc =  })
+    end,
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
