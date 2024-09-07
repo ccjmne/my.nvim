fbfd7e74de578a8bda52342e987c8b07c7b1d134
Éric NICOLAS (ccjmne) <ccjmne@gmail.com>
2024-09-07 14:12:36 +0200

    Start using mini.trailspace

--- init.lua
+++ init.lua
@@ -737,6 +737,8 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      require('mini.trailspace').setup()
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
