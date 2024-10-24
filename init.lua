b33415b193859663739a794bb8aea535be580878
Éric NICOLAS <ccjmne@gmail.com>
2024-10-24 22:23:59 +0200

    Polish telescope configuration
    Always disable preview page title.

--- init.lua
+++ init.lua
@@ -461,30 +461,32 @@ require('lazy').setup({
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
+          -- stylua: ignore start
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map('gd', function() require('telescope.builtin').lsp_definitions { preview_title = '' } end, '[G]oto [D]efinition')
 
           -- Find references for the word under your cursor.
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map('gr', function() require('telescope.builtin').lsp_references { preview_title = '' } end, '[G]oto [R]eferences')
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('gI', function() require('telescope.builtin').lsp_implementations { preview_title = '' } end, '[G]oto [I]mplementation')
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+          map('<leader>D', function() require('telescope.builtin').lsp_type_definitions { preview_title = '' } end, 'Type [D]efinition')
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          map('<leader>ds', function() require('telescope.builtin').lsp_document_symbols { preview_title = '' } end, '[D]ocument [S]ymbols')
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          map('<leader>ws', function() require('telescope.builtin').lsp_dynamic_workspace_symbols { preview_title = '' } end, '[W]orkspace [S]ymbols')
+          -- stylua: ignore end
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -89,40 +89,33 @@ return { -- Fuzzy Finder (files, lsp, etc)
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
 
-      -- stylua: ignore start
-      vim.keymap.set('n', '<leader>sh', function() builtin.help_tags { preview_title = '' } end, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', function() builtin.keymaps { preview_title = '' } end, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', function() builtin.find_files { preview_title = '' } end, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', function() builtin.builtin { preview_title = '' } end, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', function() builtin.grep_string { preview_title = '' } end, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', function() builtin.live_grep { preview_title = '' } end, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', function() builtin.diagnostics { preview_title = '' } end, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', function() builtin.resume { preview_title = '' } end, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s-', function() builtin.oldfiles { preview_title = '' } end, { desc = '[S]earch Recent Files' })
-      vim.keymap.set('n', '<leader><leader>', function() builtin.buffers { preview_title = '' } end, { desc = '[ ] Find existing buffers' })
+    -- stylua: ignore start
+      vim.keymap.set('n', '<leader>sh', function() builtin.help_tags { preview_title = '' } end,                               { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sk', function() builtin.keymaps { preview_title = '' } end,                                 { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<leader>sf', function() builtin.find_files { preview_title = '' } end,                              { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>ss', function() builtin.builtin { preview_title = '' } end,                                 { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>sw', function() builtin.grep_string { preview_title = '' } end,                             { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', function() builtin.live_grep { preview_title = '' } end,                               { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sd', function() builtin.diagnostics { preview_title = '' } end,                             { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sr', function() builtin.resume { preview_title = '' } end,                                  { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>-',  function() builtin.oldfiles { preview_title = '', prompt_title = 'Recent Files' } end, { desc = '[S]earch Recent Files' })
+      vim.keymap.set('n', '<leader>/',  function () builtin.current_buffer_fuzzy_find { preview_title = '' } end,              { desc = '[/] Fuzzily search in current buffer' })
+      vim.keymap.set('n', '<leader><leader>', function() builtin.buffers { preview_title = '' } end,                           { desc = '[ ] Find existing buffers' })
     -- stylua: ignore end
 
     vim.keymap.set('n', '<leader>s.f', function()
-      builtin.find_files { find_command = { 'rg', '--files', '--hidden', '--glob=!.git' } }
+      builtin.find_files { prompt_title = 'Find Hidden Files', find_command = { 'rg', '--files', '--hidden', '--glob=!.git' }, preview_title = '' }
     end, { desc = '[S]earch [.]idden [F]iles' })
     vim.keymap.set('n', '<leader>s.g', function()
-      builtin.live_grep { additional_args = { '--hidden', '--glob=!.git' }, preview_title = '' }
+      builtin.live_grep { prompt_title = 'Live Grep in Hidden Files', additional_args = { '--hidden', '--glob=!.git' }, preview_title = '' }
     end, { desc = '[S]earch [.]idden files by [G]rep' })
-
-    -- Slightly advanced example of overriding default behavior and theme
-    vim.keymap.set('n', '<leader>/', function()
-      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-      })
-    end, { desc = '[/] Fuzzily search in current buffer' })
 
     -- It's also possible to pass additional configuration options.
     --  See `:help telescope.builtin.live_grep()` for information about particular keys
     vim.keymap.set('n', '<leader>s/', function()
       builtin.live_grep {
         grep_open_files = true,
+        preview_title = '',
         prompt_title = 'Live Grep in Open Files',
       }
     end, { desc = '[S]earch [/] in Open Files' })
@@ -135,6 +128,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sm', function()
       -- TODO: figure out an elegant way to not have this hard-coded
       builtin.find_files { cwd = vim.env.HOME .. '/git/dotfiles', preview_title = '', find_command = { 'rg', '--files', '--hidden', '--glob=!.git' } }
-    end, { desc = '[S]earch [M]y dotfiles' })
+    end, { desc = '[S]earch [M]y Dotfiles' })
   end,
 }
